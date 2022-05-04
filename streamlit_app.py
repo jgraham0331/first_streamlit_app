1c52f965020b8c5c4e928e4289d3365d44db06b7
Jason Graham <jgraham0331@gmail.com>
2022-05-04 08:27:18 -0500

    Added pre-selected items to fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add a pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
