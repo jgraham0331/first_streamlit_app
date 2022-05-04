017c47ecb6b26b50b5bab3af2da5442db6c14f56
Jason Graham <jgraham0331@gmail.com>
2022-05-04 08:25:29 -0500

    added pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Add a pick list
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
