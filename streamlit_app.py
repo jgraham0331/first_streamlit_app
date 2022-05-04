97ea5b038350df3951457b3aaf397d730027cb54
Jason Graham <jgraham0331@gmail.com>
2022-05-04 08:26:20 -0500

    Reindexed fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# Reindex fruit list
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Add a pick list
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
