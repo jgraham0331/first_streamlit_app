1090c11066472219f96b4ab55e98c863f3997d91
Jason Graham <jgraham0331@gmail.com>
2022-05-12 09:01:15 -0500

    first snowflake query

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,6 @@ import pandas as pd
 import requests
 import snowflake.connector
 
-# Snowflake Setup
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
@@ -42,3 +34,11 @@ fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_cho
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output to screen
 streamlit.dataframe(fruityvice_normalized)
+
+# Snowflake Setup
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)
